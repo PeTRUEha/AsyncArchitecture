a06cbb15f87b5e3f0b7b564fe64a2564a4ee3a31
pvasilyev001 <>
2023-08-13 19:56:09 +0300

    started hw2

---
+++ auth_service/app/auth/auth_bearer.py
@@ -0,0 +1,29 @@
+"""The goal of this file is to check whether the reques tis authorized or not [ verification of the proteced route]"""
+
+from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from .auth_handler import decodeJWT
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials and credentials.scheme == "Bearer" and self.verify_jwt(credentials.credentials):
+            return True
+        else:
+            return False
+
+    def verify_jwt(self, jwtoken: str) -> bool:
+        isTokenValid: bool = False
+
+        try:
+            payload = decodeJWT(jwtoken)
+        except:
+            payload = None
+        if payload:
+            isTokenValid = True
+        return isTokenValid
