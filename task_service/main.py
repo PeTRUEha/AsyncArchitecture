f7f76b4cf5cd67f64ec2f36943c91bada4ee7df3
pvasilyev001 <>
2023-08-17 17:07:56 +0300

    auth endpoint works

--- task_service/main.py
+++ task_service/main.py
@@ -1,15 +1,20 @@
 from typing import Annotated
-from fastapi.responses import RedirectResponse
-from fastapi import FastAPI, Body, Depends
+import uvicorn
+import httpx
+from fastapi import FastAPI, Depends
 from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer
 
 app = FastAPI()
 
 
 def confirm_authenticity(request: Request):
     # Не работает. Не смог разобраться, как вычленить данные для авторизации для отправки на SSO сервис
-    if RedirectResponse(url='http://127.0.0.1:8000/confirm_authenticity'):
-        return
+    headers = {"Authorization": request.headers.get("Authorization")}
+    responce = httpx.get(url='http://127.0.0.1:8000/confirm_authenticity',
+                             headers=headers)
+    if responce.status_code == 200:
+        return True
     else:
         raise HTTPException(status_code=403, detail="Invalid token or expired token.")
 
@@ -18,6 +23,10 @@ def confirm_authenticity(request: Request):
 
 
 @app.post("/hey", tags=["user"])
-async def create_task(request: Request):
+async def create_task(request: Request, _: Annotated[bool, Depends(HTTPBearer())]):
     confirm_authenticity(request)
-    return {"res": 'good'}
+    return {"res": 'god'}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001)
